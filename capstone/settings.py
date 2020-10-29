4e3c00e27d5f36143348b309a1a0d9fee638c14c
John Kevin Francisco <johnkev.francisco@gmail.com>
2020-10-29 11:31:44 +0800

    layout and index

--- capstone/settings.py
+++ capstone/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'films',
+    'livereload',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- films/views.py
+++ films/views.py
@@ -1,6 +1,20 @@
+# The API Key is set in .vscode/launch.json for debugging
+import os, json, urllib
+
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
-# Create your views here.
-
 def index(request):
-    return HttpResponse("Commence Capstone: Films.... Start!!!")
+    greeting = "Commence Capstone: Films"
+    api_key = os.environ.get("TMDB_API_KEY")
+
+    # url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+    # response = urllib.request.urlopen(url) 
+    # data = json.loads(response.read())
+
+    context =  {
+        'greeting': greeting,
+        'api_key': api_key,
+        # 'data': data,
+    }
+    
+    return render(request, "films/index.html", context)
