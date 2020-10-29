f4182b4be7d1f25a339d573c0d03cebacb7721d4
John Kevin Francisco <johnkev.francisco@gmail.com>
2020-10-29 09:20:27 +0800

    define urls for films app

--- films/views.py
+++ films/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Commence Capstone: Films.... Start!!!")
