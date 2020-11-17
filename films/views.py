8068429946b343e15764845f46beb67effb2f054
John Kevin Francisco <johnkev.francisco@gmail.com>
2020-11-17 18:51:17 +0800

    start react components

--- films/views.py
+++ films/views.py
@@ -4,7 +4,7 @@ import os, json, urllib
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 def index(request):
-    greeting = "Commence Capstone: Films"
+    heading_title = "ShowPot!"
     api_key = os.environ.get("TMDB_API_KEY")
 
     # url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
@@ -12,9 +12,10 @@ def index(request):
     # data = json.loads(response.read())
 
     context =  {
-        'greeting': greeting,
+        'heading_title': heading_title,
         'api_key': api_key,
         # 'data': data,
     }
     
     return render(request, "films/index.html", context)
+
